8c5ed2ec1f6094208dd8a2b166b2e460f5db9d22
mmoss18 <mmoss200511@gmail.com>
2024-09-10 22:11:43 -0700

    fixed asmFunc.s files
    replaced inigo montoya with michael moss and inserted ADD r0, r0, r1 where specified.

--- Lab-02-starter/firmware/src/asmFunc.s
+++ Lab-02-starter/firmware/src/asmFunc.s
@@ -54,7 +54,7 @@ asmFunc:
 
     
     /*** STUDENTS: Place your code BELOW this line!!! **************/
-   ADD r0, r0, r1 
+    ADD r0, r0, r1
     
     /*** STUDENTS: Place your code ABOVE this line!!! **************/
     
